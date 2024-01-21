0e941e17dae8d8aa7def127791acae4de96ad58d
Eunjin <155360562+eunjin210@users.noreply.github.com>
2024-01-22 00:11:16 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,15 +1,11 @@
 N, M = map(int, input().split())
 arr =[]
+a = N*[0]
 for i in range(N):
     arr.append(i+1)
-
 for _ in range(M):
     i, j = map(int, input().split())
-    for right in range((j-i)//2+1):
-        left = j-1-right
-        temp = arr[left]
-        arr[left] = arr[right+i-1]
-        arr[right+i-1] = temp
+    arr[i-1:j]= arr[i-1:j][::-1]
 
 for i in range(N):
     print(arr[i], end =' ')
